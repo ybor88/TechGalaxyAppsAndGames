4d907714b2289508b538174d317fef21ed03f629
ybor88 <ybor88@users.noreply.github.com>
2026-09-22 10:42:31 +0200

    add

--- SoniqAI/app/src/main/java/com/soniqai/app/SoniqAiApp.kt
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Roberto Di Flumeri
-package com.soniqai.app
-
-import android.app.Application
-
-class SoniqAiApp : Application()
--- Vanta/app/__init__.py
+++
@@ -1,1 +0,0 @@
-# Copyright (c) Roberto Di Flumeri
